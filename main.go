92143beabc8fafec482f54989c25e0b42934b4b3
krishnanunni <krishnanunni.n.menon@gmail.com>
2023-09-19 13:55:28 +0530

    generate update sql

--- main.go
+++ main.go
@@ -15,9 +15,10 @@ type OpLogEntry struct {
 	Operation string                 `json:"op"`
 	Namespace string                 `json:"ns"`
 	Object    map[string]interface{} `json:"o"`
+	Object2   map[string]interface{} `json:"o2"`
 }
 
-func GenerateInsertSql(oplog string) (string, error) {
+func GenerateSql(oplog string) (string, error) {
 	var opLogEntry OpLogEntry
 	if err := json.Unmarshal([]byte(oplog), &opLogEntry); err != nil {
 		return "", err
@@ -25,25 +26,65 @@ func GenerateInsertSql(oplog string) (string, error) {
 
 	switch opLogEntry.Operation {
 	case "i":
-		sql := fmt.Sprintf("INSERT INTO %s", opLogEntry.Namespace)
-		columnNames := make([]string, 0, len(opLogEntry.Object))
-		for columnName := range opLogEntry.Object {
-			columnNames = append(columnNames, columnName)
-		}
-		sort.Strings(columnNames)
-
-		columnValues := make([]string, 0, len(opLogEntry.Object))
-		for _, columnName := range columnNames {
-			columnValues = append(columnValues, getColumnValue(opLogEntry.Object[columnName]))
-		}
-		sql = fmt.Sprintf("%s (%s) VALUES (%s);", sql, strings.Join(columnNames, ", "), strings.Join(columnValues, ", "))
-		return sql, nil
+		return generateInsertSql(opLogEntry)
+	case "u":
+		return generateupdateSql(opLogEntry)
 	}
 
 	return "", nil
 }
 
-func getColumnValue(columnValue interface{}) string {
+func generateInsertSql(opLogEntry OpLogEntry) (string, error) {
+	sql := fmt.Sprintf("INSERT INTO %s", opLogEntry.Namespace)
+	columnNames := make([]string, 0, len(opLogEntry.Object))
+	for columnName := range opLogEntry.Object {
+		columnNames = append(columnNames, columnName)
+	}
+	sort.Strings(columnNames)
+
+	columnValues := make([]string, 0, len(opLogEntry.Object))
+	for _, columnName := range columnNames {
+		columnValues = append(columnValues, columnValueToString(opLogEntry.Object[columnName]))
+	}
+	sql = fmt.Sprintf("%s (%s) VALUES (%s);", sql, strings.Join(columnNames, ", "), strings.Join(columnValues, ", "))
+	return sql, nil
+}
+
+func generateupdateSql(opLogEntry OpLogEntry) (string, error) {
+	sql := fmt.Sprintf("UPDATE %s SET", opLogEntry.Namespace)
+	diffMap, ok := opLogEntry.Object["diff"].(map[string]interface{})
+	if !ok {
+		return "", fmt.Errorf("Invalid Update Oplog")
+	}
+
+	if setMap, ok := diffMap["u"].(map[string]interface{}); ok {
+		columnValues := make([]string, 0, len(setMap))
+		for columnName, columnValue := range setMap {
+			columnValues = append(columnValues, fmt.Sprintf("%s = %s", columnName, columnValueToString(columnValue)))
+		}
+		sort.Strings(columnValues)
+		sql = fmt.Sprintf("%s %s", sql, strings.Join(columnValues, ", "))
+	} else if unsetMap, ok := diffMap["d"].(map[string]interface{}); ok {
+		columnValues := make([]string, 0, len(unsetMap))
+		for columnName := range unsetMap {
+			columnValues = append(columnValues, fmt.Sprintf("%s = NULL", columnName))
+		}
+		sort.Strings(columnValues)
+		sql = fmt.Sprintf("%s %s", sql, strings.Join(columnValues, ", "))
+	} else {
+		return "", fmt.Errorf("Invalid Update Oplog")
+	}
+
+	whereColumnValues := make([]string, 0, len(opLogEntry.Object2))
+	for columnName, columnValue := range opLogEntry.Object2 {
+		whereColumnValues = append(whereColumnValues, fmt.Sprintf("%s = %s", columnName, columnValueToString(columnValue)))
+	}
+
+	sql = fmt.Sprintf("%s WHERE %s;", sql, strings.Join(whereColumnValues, " AND "))
+	return sql, nil
+}
+
+func columnValueToString(columnValue interface{}) string {
 	switch columnValue.(type) {
 	case int, int8, int16, int32, int64, float32, float64:
 		return fmt.Sprintf("%v", columnValue)
--- main_test.go
+++ main_test.go
@@ -32,16 +32,77 @@ func TestGenerateInsertSql(t *testing.T) {
 			want:    "INSERT INTO test.student (_id, date_of_birth, is_graduated, name, roll_no) VALUES ('635b79e231d82a8ab1de863b', '2000-01-30', false, 'Selena Miller', 51);",
 			wantErr: false,
 		},
+		{
+			name: "Update operation - set",
+			oplog: `{
+				"op": "u",
+				"ns": "test.student",
+				"o": {
+				   "$v": 2,
+				   "diff": {
+					  "u": {
+						 "is_graduated": true
+					  }
+				   }
+				},
+				 "o2": {
+				   "_id": "635b79e231d82a8ab1de863b"
+				}
+			}`,
+			want:    "UPDATE test.student SET is_graduated = true WHERE _id = '635b79e231d82a8ab1de863b';",
+			wantErr: false,
+		},
+		{
+			name: "Update operation - set with multiple columns",
+			oplog: `{
+				"op": "u",
+				"ns": "test.student",
+				"o": {
+				   "$v": 2,
+				   "diff": {
+					  "u": {
+						 "is_graduated": true,
+						 "roll_no": 21
+					  }
+				   }
+				},
+				 "o2": {
+				   "_id": "635b79e231d82a8ab1de863b"
+				}
+			}`,
+			want:    "UPDATE test.student SET is_graduated = true, roll_no = 21 WHERE _id = '635b79e231d82a8ab1de863b';",
+			wantErr: false,
+		},
+		{
+			name: "Update operation - unset",
+			oplog: `{
+				"op": "u",
+				"ns": "test.student",
+				"o": {
+				   "$v": 2,
+				   "diff": {
+					  "d": {
+						 "roll_no": false
+					  }
+				   }
+				},
+				"o2": {
+				   "_id": "635b79e231d82a8ab1de863b"
+				}
+			}`,
+			want:    "UPDATE test.student SET roll_no = NULL WHERE _id = '635b79e231d82a8ab1de863b';",
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := GenerateInsertSql(tt.oplog)
+			got, err := GenerateSql(tt.oplog)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("GenerateInsertSql() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("GenerateSql() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if got != tt.want {
-				t.Errorf("GenerateInsertSql() = %v, want %v", got, tt.want)
+				t.Errorf("GenerateSql() = %v, want %v", got, tt.want)
 			}
 		})
 	}
